3e54d40799d736c2cb085a378367cbdde0b53f64
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-02-04 17:43:58 -0500

    logit transforms

--- R/utility.R
+++ R/utility.R
@@ -5,3 +5,15 @@
 #' @param muhat same length vectorm conventionally the estimate
 #' @export
 unit_rmse <- function(mu = truth, muhat) sqrt(mean((mu - muhat)^2))
+
+
+#' Inverse logit function (xb to  p)
+#'
+#'
+expit <- function(x) (1 + exp(-x)) ^ (-1)
+
+#' Logit function (p to xb)
+#'
+#' log(p / (1 - p))
+#'
+logit <- function(x) log(x) - log(1 - x)
